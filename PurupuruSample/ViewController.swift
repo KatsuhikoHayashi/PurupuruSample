cd0e74f9651baeb4882653bad5e187b59eec55a9
hayashidesu <katsu-h@w4.dion.ne.jp>
2015-07-07 15:56:10 +0900

    Initial Commit

---
+++ PurupuruSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PurupuruSample
+//
+//  Created by 林克彦 on 2015/07/07.
+//  Copyright (c) 2015年 Hayashidesu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
